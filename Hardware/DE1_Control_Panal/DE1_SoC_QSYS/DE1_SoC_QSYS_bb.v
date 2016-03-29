e17c3a0506cde20357fcdf60b08a4b7b22322b56
ebradley6 <esbradley@live.com>
2016-03-28 21:24:08 -0400

    Control Panel
    The control panel example recompiled. Use Quartus' programmer to program
    the FPGA with the time limited .sof file.

---
+++ Hardware/DE1_Control_Panal/DE1_SoC_QSYS/DE1_SoC_QSYS_bb.v
@@ -0,0 +1,234 @@
+
+module DE1_SoC_QSYS (
+	vid_clk_to_the_alt_vip_cti_0,
+	vid_data_to_the_alt_vip_cti_0,
+	overflow_from_the_alt_vip_cti_0,
+	vid_datavalid_to_the_alt_vip_cti_0,
+	vid_locked_to_the_alt_vip_cti_0,
+	alt_vip_itc_0_clocked_video_vid_clk,
+	alt_vip_itc_0_clocked_video_vid_data,
+	alt_vip_itc_0_clocked_video_underflow,
+	alt_vip_itc_0_clocked_video_vid_datavalid,
+	alt_vip_itc_0_clocked_video_vid_v_sync,
+	alt_vip_itc_0_clocked_video_vid_h_sync,
+	alt_vip_itc_0_clocked_video_vid_f,
+	alt_vip_itc_0_clocked_video_vid_h,
+	alt_vip_itc_0_clocked_video_vid_v,
+	clk_50,
+	reset_n,
+	clk_sdram_clk,
+	clk_vga_clk,
+	hps_0_h2f_reset_reset_n_reset_n,
+	hps_io_hps_io_emac1_inst_TX_CLK,
+	hps_io_hps_io_emac1_inst_TXD0,
+	hps_io_hps_io_emac1_inst_TXD1,
+	hps_io_hps_io_emac1_inst_TXD2,
+	hps_io_hps_io_emac1_inst_TXD3,
+	hps_io_hps_io_emac1_inst_RXD0,
+	hps_io_hps_io_emac1_inst_MDIO,
+	hps_io_hps_io_emac1_inst_MDC,
+	hps_io_hps_io_emac1_inst_RX_CTL,
+	hps_io_hps_io_emac1_inst_TX_CTL,
+	hps_io_hps_io_emac1_inst_RX_CLK,
+	hps_io_hps_io_emac1_inst_RXD1,
+	hps_io_hps_io_emac1_inst_RXD2,
+	hps_io_hps_io_emac1_inst_RXD3,
+	hps_io_hps_io_qspi_inst_IO0,
+	hps_io_hps_io_qspi_inst_IO1,
+	hps_io_hps_io_qspi_inst_IO2,
+	hps_io_hps_io_qspi_inst_IO3,
+	hps_io_hps_io_qspi_inst_SS0,
+	hps_io_hps_io_qspi_inst_CLK,
+	hps_io_hps_io_sdio_inst_CMD,
+	hps_io_hps_io_sdio_inst_D0,
+	hps_io_hps_io_sdio_inst_D1,
+	hps_io_hps_io_sdio_inst_CLK,
+	hps_io_hps_io_sdio_inst_D2,
+	hps_io_hps_io_sdio_inst_D3,
+	hps_io_hps_io_usb1_inst_D0,
+	hps_io_hps_io_usb1_inst_D1,
+	hps_io_hps_io_usb1_inst_D2,
+	hps_io_hps_io_usb1_inst_D3,
+	hps_io_hps_io_usb1_inst_D4,
+	hps_io_hps_io_usb1_inst_D5,
+	hps_io_hps_io_usb1_inst_D6,
+	hps_io_hps_io_usb1_inst_D7,
+	hps_io_hps_io_usb1_inst_CLK,
+	hps_io_hps_io_usb1_inst_STP,
+	hps_io_hps_io_usb1_inst_DIR,
+	hps_io_hps_io_usb1_inst_NXT,
+	hps_io_hps_io_spim1_inst_CLK,
+	hps_io_hps_io_spim1_inst_MOSI,
+	hps_io_hps_io_spim1_inst_MISO,
+	hps_io_hps_io_spim1_inst_SS0,
+	hps_io_hps_io_uart0_inst_RX,
+	hps_io_hps_io_uart0_inst_TX,
+	hps_io_hps_io_i2c0_inst_SDA,
+	hps_io_hps_io_i2c0_inst_SCL,
+	hps_io_hps_io_i2c1_inst_SDA,
+	hps_io_hps_io_i2c1_inst_SCL,
+	hps_io_hps_io_gpio_inst_GPIO09,
+	hps_io_hps_io_gpio_inst_GPIO35,
+	hps_io_hps_io_gpio_inst_GPIO40,
+	hps_io_hps_io_gpio_inst_GPIO41,
+	hps_io_hps_io_gpio_inst_GPIO48,
+	hps_io_hps_io_gpio_inst_GPIO53,
+	hps_io_hps_io_gpio_inst_GPIO54,
+	hps_io_hps_io_gpio_inst_GPIO61,
+	out_port_from_the_i2c_scl,
+	bidir_port_to_and_from_the_i2c_sda,
+	key_external_connection_export,
+	ledr_external_connection_export,
+	memory_mem_a,
+	memory_mem_ba,
+	memory_mem_ck,
+	memory_mem_ck_n,
+	memory_mem_cke,
+	memory_mem_cs_n,
+	memory_mem_ras_n,
+	memory_mem_cas_n,
+	memory_mem_we_n,
+	memory_mem_reset_n,
+	memory_mem_dq,
+	memory_mem_dqs,
+	memory_mem_dqs_n,
+	memory_mem_odt,
+	memory_mem_dm,
+	memory_oct_rzqin,
+	pll_0_locked_export,
+	pll_audio_locked_export,
+	zs_addr_from_the_sdram,
+	zs_ba_from_the_sdram,
+	zs_cas_n_from_the_sdram,
+	zs_cke_from_the_sdram,
+	zs_cs_n_from_the_sdram,
+	zs_dq_to_and_from_the_sdram,
+	zs_dqm_from_the_sdram,
+	zs_ras_n_from_the_sdram,
+	zs_we_n_from_the_sdram,
+	spi_0_external_MISO,
+	spi_0_external_MOSI,
+	spi_0_external_SCLK,
+	spi_0_external_SS_n,
+	sw_external_connection_export,
+	out_port_from_the_td_reset_n,
+	in_port_to_the_td_status,
+	uart_external_connection_rxd,
+	uart_external_connection_txd);	
+
+	input		vid_clk_to_the_alt_vip_cti_0;
+	input	[7:0]	vid_data_to_the_alt_vip_cti_0;
+	output		overflow_from_the_alt_vip_cti_0;
+	input		vid_datavalid_to_the_alt_vip_cti_0;
+	input		vid_locked_to_the_alt_vip_cti_0;
+	input		alt_vip_itc_0_clocked_video_vid_clk;
+	output	[23:0]	alt_vip_itc_0_clocked_video_vid_data;
+	output		alt_vip_itc_0_clocked_video_underflow;
+	output		alt_vip_itc_0_clocked_video_vid_datavalid;
+	output		alt_vip_itc_0_clocked_video_vid_v_sync;
+	output		alt_vip_itc_0_clocked_video_vid_h_sync;
+	output		alt_vip_itc_0_clocked_video_vid_f;
+	output		alt_vip_itc_0_clocked_video_vid_h;
+	output		alt_vip_itc_0_clocked_video_vid_v;
+	input		clk_50;
+	input		reset_n;
+	output		clk_sdram_clk;
+	output		clk_vga_clk;
+	output		hps_0_h2f_reset_reset_n_reset_n;
+	output		hps_io_hps_io_emac1_inst_TX_CLK;
+	output		hps_io_hps_io_emac1_inst_TXD0;
+	output		hps_io_hps_io_emac1_inst_TXD1;
+	output		hps_io_hps_io_emac1_inst_TXD2;
+	output		hps_io_hps_io_emac1_inst_TXD3;
+	input		hps_io_hps_io_emac1_inst_RXD0;
+	inout		hps_io_hps_io_emac1_inst_MDIO;
+	output		hps_io_hps_io_emac1_inst_MDC;
+	input		hps_io_hps_io_emac1_inst_RX_CTL;
+	output		hps_io_hps_io_emac1_inst_TX_CTL;
+	input		hps_io_hps_io_emac1_inst_RX_CLK;
+	input		hps_io_hps_io_emac1_inst_RXD1;
+	input		hps_io_hps_io_emac1_inst_RXD2;
+	input		hps_io_hps_io_emac1_inst_RXD3;
+	inout		hps_io_hps_io_qspi_inst_IO0;
+	inout		hps_io_hps_io_qspi_inst_IO1;
+	inout		hps_io_hps_io_qspi_inst_IO2;
+	inout		hps_io_hps_io_qspi_inst_IO3;
+	output		hps_io_hps_io_qspi_inst_SS0;
+	output		hps_io_hps_io_qspi_inst_CLK;
+	inout		hps_io_hps_io_sdio_inst_CMD;
+	inout		hps_io_hps_io_sdio_inst_D0;
+	inout		hps_io_hps_io_sdio_inst_D1;
+	output		hps_io_hps_io_sdio_inst_CLK;
+	inout		hps_io_hps_io_sdio_inst_D2;
+	inout		hps_io_hps_io_sdio_inst_D3;
+	inout		hps_io_hps_io_usb1_inst_D0;
+	inout		hps_io_hps_io_usb1_inst_D1;
+	inout		hps_io_hps_io_usb1_inst_D2;
+	inout		hps_io_hps_io_usb1_inst_D3;
+	inout		hps_io_hps_io_usb1_inst_D4;
+	inout		hps_io_hps_io_usb1_inst_D5;
+	inout		hps_io_hps_io_usb1_inst_D6;
+	inout		hps_io_hps_io_usb1_inst_D7;
+	input		hps_io_hps_io_usb1_inst_CLK;
+	output		hps_io_hps_io_usb1_inst_STP;
+	input		hps_io_hps_io_usb1_inst_DIR;
+	input		hps_io_hps_io_usb1_inst_NXT;
+	output		hps_io_hps_io_spim1_inst_CLK;
+	output		hps_io_hps_io_spim1_inst_MOSI;
+	input		hps_io_hps_io_spim1_inst_MISO;
+	output		hps_io_hps_io_spim1_inst_SS0;
+	input		hps_io_hps_io_uart0_inst_RX;
+	output		hps_io_hps_io_uart0_inst_TX;
+	inout		hps_io_hps_io_i2c0_inst_SDA;
+	inout		hps_io_hps_io_i2c0_inst_SCL;
+	inout		hps_io_hps_io_i2c1_inst_SDA;
+	inout		hps_io_hps_io_i2c1_inst_SCL;
+	inout		hps_io_hps_io_gpio_inst_GPIO09;
+	inout		hps_io_hps_io_gpio_inst_GPIO35;
+	inout		hps_io_hps_io_gpio_inst_GPIO40;
+	inout		hps_io_hps_io_gpio_inst_GPIO41;
+	inout		hps_io_hps_io_gpio_inst_GPIO48;
+	inout		hps_io_hps_io_gpio_inst_GPIO53;
+	inout		hps_io_hps_io_gpio_inst_GPIO54;
+	inout		hps_io_hps_io_gpio_inst_GPIO61;
+	output		out_port_from_the_i2c_scl;
+	inout		bidir_port_to_and_from_the_i2c_sda;
+	input	[3:0]	key_external_connection_export;
+	output	[9:0]	ledr_external_connection_export;
+	output	[14:0]	memory_mem_a;
+	output	[2:0]	memory_mem_ba;
+	output		memory_mem_ck;
+	output		memory_mem_ck_n;
+	output		memory_mem_cke;
+	output		memory_mem_cs_n;
+	output		memory_mem_ras_n;
+	output		memory_mem_cas_n;
+	output		memory_mem_we_n;
+	output		memory_mem_reset_n;
+	inout	[31:0]	memory_mem_dq;
+	inout	[3:0]	memory_mem_dqs;
+	inout	[3:0]	memory_mem_dqs_n;
+	output		memory_mem_odt;
+	output	[3:0]	memory_mem_dm;
+	input		memory_oct_rzqin;
+	output		pll_0_locked_export;
+	output		pll_audio_locked_export;
+	output	[12:0]	zs_addr_from_the_sdram;
+	output	[1:0]	zs_ba_from_the_sdram;
+	output		zs_cas_n_from_the_sdram;
+	output		zs_cke_from_the_sdram;
+	output		zs_cs_n_from_the_sdram;
+	inout	[15:0]	zs_dq_to_and_from_the_sdram;
+	output	[1:0]	zs_dqm_from_the_sdram;
+	output		zs_ras_n_from_the_sdram;
+	output		zs_we_n_from_the_sdram;
+	input		spi_0_external_MISO;
+	output		spi_0_external_MOSI;
+	output		spi_0_external_SCLK;
+	output		spi_0_external_SS_n;
+	input	[9:0]	sw_external_connection_export;
+	output		out_port_from_the_td_reset_n;
+	input	[1:0]	in_port_to_the_td_status;
+	input		uart_external_connection_rxd;
+	output		uart_external_connection_txd;
+endmodule
